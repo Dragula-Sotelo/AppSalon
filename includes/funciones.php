248313e235f8a756617e1865ebb67c8f2de9ef56
Dragula-Sotelo <ninosotelo60@gmail.com>
2022-03-24 14:16:16 -0600

    first commit

---
+++ includes/funciones.php
@@ -0,0 +1,40 @@
+<?php 
+
+function obtenerServicios() : array{
+    try {
+        //Importar una conexión
+        require 'database.php';
+
+        //Escribir el código SQL
+        $sql = "SELECT * FROM servicios;";
+        $consulta = mysqli_query($db, $sql);
+        
+        //Arreglo vacío
+        $servicios = [];
+        $i = 0;
+
+        //Obtener los resultados
+        // echo "<pre>";
+        // var_dump(mysqli_fetch_assoc($consulta));
+        // echo "</pre>";
+        while($row = mysqli_fetch_assoc($consulta)){
+            $servicios[$i]['id'] = $row['id'];
+            $servicios[$i]['nombre'] = $row['nombre'];
+            $servicios[$i]['precio'] = $row['precio'];
+
+            $i++;
+        }
+        // echo"<pre>";
+        // var_dump(json_encode($servicios));
+        // echo"</pre>";
+
+        return $servicios;
+
+    } catch (\Throwable $th) {
+        //throw $th;
+
+        var_dump($th);
+    }
+}
+
+obtenerServicios();
